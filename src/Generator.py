690d3ac6cbefdc3545ab146a3ac84c3f1f066330
Vincent Kurta <vincent@swapcard.com>
2018-04-24 10:05:43 +0100

    updated text graph generator : edges names, default value for weight (1.0), removed self.graphs in favor of a local variable, typing

--- src/Generator.py
+++ src/Generator.py
@@ -1,3 +1,5 @@
+from typing import List
+
 from src.TextGraph import TextGraph
 from src.utils import find_edge
 
@@ -5,25 +7,27 @@ from src.utils import find_edge
 class Generator:
     def __init__(self, n):
         self.n = n
-        self.graphs = []
 
-    def __get_ngrams(self, doc):
+    def __get_ngrams(self, doc: str):
         return [doc[x:x + self.n:1] for x in range(len(doc) - self.n + 1)]
 
-    def generate_text_graphs(self, documents, weight):
+    def __generate_text_graph(self, doc: str, weight: float):
+        graph = TextGraph()
+        ngrams = self.__get_ngrams(doc)
+        for gram in ngrams:
+            if gram not in [v["name"] for v in graph.vs]:
+                graph.add_vertex(name=gram)
+
+        for n1, n2 in zip(ngrams, ngrams[1:]):
+            e = find_edge(graph, n1, n2)
+            if e:
+                e["weight"] += weight
+            else:
+                graph.add_edge(n1, n2, name=n1 + ' ' + n2, weight=weight)
+        return graph
+
+    def generate_text_graphs(self, documents: List[str], weight: float=1.0):
+        graphs = []
         for doc in documents:
-            graph = TextGraph()
-            ngrams = self.__get_ngrams(doc)
-            for gram in ngrams:
-                if gram not in [v["name"] for v in graph.vs]:
-                    graph.add_vertex(name=gram)
-
-            for n1, n2 in zip(ngrams, ngrams[1:]):
-                e = find_edge(graph, n1, n2)
-                if e:
-                    e["weight"] += weight
-                else:
-                    graph.add_edge(n1, n2, name=n1 + n2, weight=weight)
-
-            self.graphs.append(graph)
-        return self.graphs
+            graphs.append(self.__generate_text_graph(doc, weight))
+        return graphs
