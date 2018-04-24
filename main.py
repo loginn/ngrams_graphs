2d07bdc29d58b77c08584b44dad5a3f6b85b18b2
Vincent Kurta <vincent@swapcard.com>
2018-04-24 10:06:47 +0100

    updated main with a different test

--- main.py
+++ main.py
@@ -1,14 +1,21 @@
 from src.Generator import Generator
 from src.utils import generate_model_graph
+from src.comparison import *
 
-n = 3
-data = ['head of marketing', 'marketing manager', 'marketing marketing']
+n = 2
+data = ['head of marketing', 'marketing manager', 'marketing consultant', 'marketing']
+test = ['marketing manager', 'market analysis', 'construction worker']
 
 generator = Generator(n)
 
-graphs = generator.generate_text_graphs(data, weight=1.0)
+graphs = generator.generate_text_graphs(data)
+test_graphs = generator.generate_text_graphs(test)
 
-model = generate_model_graph(graphs, lr=0.5)
+g1 = generate_model_graph(graphs)
 
-for e in model.es:
-    print(e["weight"])
+for idx, g2 in enumerate(test_graphs):
+    print(test[idx])
+    print('\tsize similarity :\t\t\t\t', size_similarity(g1, g2))
+    print('\tcontainment_similarity :\t\t', containment_similarity(g1, g2))
+    print('\tvalue_similarity :\t\t\t\t', value_similarity(g1, g2))
+    print('\tnormalized_value_similarity :\t', normalized_value_similarity(g1, g2))
