c38173503886543729e3a4ef4b73dd1e1b70081f
Vincent Kurta <vincent@swapcard.com>
2018-04-24 10:07:28 +0100

    typo

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ g1 = generate_model_graph(graphs)
 
 for idx, g2 in enumerate(test_graphs):
     print(test[idx])
-    print('\tsize similarity :\t\t\t\t', size_similarity(g1, g2))
+    print('\tsize_similarity :\t\t\t\t', size_similarity(g1, g2))
     print('\tcontainment_similarity :\t\t', containment_similarity(g1, g2))
     print('\tvalue_similarity :\t\t\t\t', value_similarity(g1, g2))
     print('\tnormalized_value_similarity :\t', normalized_value_similarity(g1, g2))
