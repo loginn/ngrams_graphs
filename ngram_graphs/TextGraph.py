3e3f093a85c59bf0db8babe4dabb201b898a1084
Vincent Kurta <vincent@swapcard.com>
2018-05-09 09:55:13 +0100

    Passing the weight of the other edge directly instead of the other edge itself

--- ngram_graphs/TextGraph.py
+++ ngram_graphs/TextGraph.py
@@ -8,8 +8,8 @@ class TextGraph(Graph):
         super().__init__()
 
     @staticmethod
-    def __calc_new_weight(s_edge, o_edge, learning_factor) -> float:
-        return s_edge["weight"] + ((o_edge["weight"] - s_edge["weight"]) * learning_factor)
+    def __calc_new_weight(s_edge, weight, learning_factor) -> float:
+        return s_edge["weight"] + ((weight - s_edge["weight"]) * learning_factor)
 
     def __add_unknown_vertices(self, other):
         for vertex in other.vs:
@@ -25,9 +25,12 @@ class TextGraph(Graph):
         for o_edge in other.es:
             s_edge = next(iter([e for e in self.es if e["name"] == o_edge["name"]]), None)
             if s_edge is not None:
-                s_edge["weight"] = self.__calc_new_weight(s_edge, o_edge, learning_factor)
+                s_edge["weight"] = self.__calc_new_weight(s_edge, o_edge["weight"], learning_factor)
             else:
                 self.__add_unknown_edge(other, o_edge, learning_factor)
+        for s_edge in self.es:
+            if s_edge["name"] not in other.es["name"]:
+                s_edge["weight"] = self.__calc_new_weight(s_edge, 0, learning_factor)
 
     def update(self, other: 'TextGraph', learning_factor):
         self.__add_unknown_vertices(other)
