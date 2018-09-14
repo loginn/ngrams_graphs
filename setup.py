26dbfc200316010e777090678d7e1db7f01cf613
Vincent Kurta <vincent@swapcard.com>
2018-09-14 11:32:31 +0100

    updated setup

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='ngram_graphs',
-      version='0.1.5',
+      version='0.1.6',
       description='An igraph based ngram graph library for NLP',
       long_description="""Library for ngram graph comparison based on papers from George Giannakopoulos and Vangelis Karkaletsis. The library uses igraph to create directed and weighted graphs from documents and provides means to compare them using the distance metrics defined in 'Giannakopoulos, George & Karkaletsis, Vangelis. (2009). N-gram graphs: Representing documents and document sets in summary system evaluation. '""",
       url='https://github.com/loginn/ngrams_graphs',
@@ -13,7 +13,7 @@ setup(name='ngram_graphs',
       keywords='ngrams graph NLP',
       author_email='vincent.kurta2@gmail.com',
       license='MIT',
-      install_requires=['python-igraph'],
+      install_requires=['python-igraph', 'networkx'],
       packages=['ngram_graphs'],
       zip_safe=False,
       test_suite='nose.collector',
