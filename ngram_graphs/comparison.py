ff4a545265aaad2215670718fee069ca557646e2
Vincent Kurta <vincent@swapcard.com>
2019-06-03 11:09:52 +0100

    Massive improvement to value similarity speed when using networkx (also improves normalized value similarity)

--- ngram_graphs/comparison.py
+++ ngram_graphs/comparison.py
@@ -1,8 +1,10 @@
+from typing import Union
+
 from ngram_graphs.TextGraph.IGraphTextGraph import IGraphTextGraph
 from ngram_graphs.TextGraph.NetworkxTextGraph import NetworkxTextGraph
 
 
-def size_similarity(g1, g2):
+def size_similarity(g1: Union[IGraphTextGraph, NetworkxTextGraph], g2: Union[IGraphTextGraph, NetworkxTextGraph]):
     if isinstance(g1, IGraphTextGraph) and isinstance(g2, IGraphTextGraph):
         s1 = len(g1.es)
         s2 = len(g2.es)
@@ -10,7 +12,7 @@ def size_similarity(g1, g2):
         s1 = len(g1.edges)
         s2 = len(g2.edges)
     else:
-        raise TypeError('Invalid Graph types')
+        raise TypeError(f'Invalid Graph types\ng1 is a {type(g1)} and g2 is a {type(g2)}')
 
     if not s1 or not s2:
         return None
@@ -18,7 +20,7 @@ def size_similarity(g1, g2):
     return min(s1, s2) / max(s1, s2)
 
 
-def containment_similarity(g1, g2):
+def containment_similarity(g1: Union[IGraphTextGraph, NetworkxTextGraph], g2: Union[IGraphTextGraph, NetworkxTextGraph]):
     if isinstance(g1, IGraphTextGraph) and isinstance(g2, IGraphTextGraph):
         s1 = len(g1.es)
         s2 = len(g2.es)
@@ -36,7 +38,7 @@ def containment_similarity(g1, g2):
 
         return len(common_edges) / max(s1, s2)
     else:
-        raise TypeError('Invalid Graph types')
+        raise TypeError(f'Invalid Graph types\ng1 is a {type(g1)} and g2 is a {type(g2)}')
 
 
 def value_ratio(e1, e2):
@@ -46,7 +48,7 @@ def value_ratio(e1, e2):
     return min(w1, w2) / max(w1, w2)
 
 
-def value_similarity(g1, g2):
+def value_similarity(g1: Union[IGraphTextGraph, NetworkxTextGraph], g2: Union[IGraphTextGraph, NetworkxTextGraph]):
     if isinstance(g1, IGraphTextGraph) and isinstance(g2, IGraphTextGraph):
         s1 = len(g1.es)
         s2 = len(g2.es)
@@ -55,16 +57,20 @@ def value_similarity(g1, g2):
         return sum(value_ratio(e[0], e[1]) for e in common_edges) / max(s1, s2)
 
     elif isinstance(g1, NetworkxTextGraph) and isinstance(g2, NetworkxTextGraph):
-        s1 = len(g1.edges)
-        s2 = len(g2.edges)
+        e1 = set(g1.edges)
+        e2 = set(g2.edges)
 
-        common_edges = [(e1, e2) for e1 in g1.edges(data=True) for e2 in g2.edges(data=True) if e1[2]['name'] == e2[2]['name']]
-        return sum(value_ratio(e[0][2], e[1][2]) for e in common_edges) / max(s1, s2)
+        s1 = len(e1)
+        s2 = len(e2)
+
+        common_edges = e1 & e2
+        common_edges = [(g1.get_edge_data(t[0], t[1]), g2.get_edge_data(t[0], t[1])) for t in common_edges]
+        return sum(value_ratio(e[0], e[1]) for e in common_edges) / max(s1, s2)
     else:
-        raise TypeError('Invalid Graph types')
+        raise TypeError(f'Invalid Graph types\ng1 is a {type(g1)} and g2 is a {type(g2)}')
 
 
-def normalized_value_similarity(g1, g2):
+def normalized_value_similarity(g1: Union[IGraphTextGraph, NetworkxTextGraph], g2: Union[IGraphTextGraph, NetworkxTextGraph]):
     s = size_similarity(g1, g2)
     v = value_similarity(g1, g2)
     if not s:
